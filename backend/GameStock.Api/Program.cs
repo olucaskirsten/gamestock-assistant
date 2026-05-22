35dd8916fd115e666597639af50a7f1d267b5605
olucaskirsten <lucaseduak@gmail.com>
2026-05-22 14:48:06 -0300

    project update

--- backend/GameStock.Api/Program.cs
+++ backend/GameStock.Api/Program.cs
@@ -11,12 +11,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(CorsPolicy, policy =>
     {
         policy
-            .WithOrigins(
-                "http://127.0.0.1:5500",
-                "http://localhost:5500",
-                "http://127.0.0.1:5501",
-                "http://localhost:5501"
-            )
+            .AllowAnyOrigin()
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
